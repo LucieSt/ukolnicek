55f8c410ea3b15383dcf1743e9854eff9a13e10f
LucieSt <strakova.91@gmail.com>
2022-10-24 16:42:39 +0200

    got data, started to work on component Task

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('funguju!');
+
+const apiUrl = "https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks"
+
+const Task = (props) => {
+    const { id, name, due, done } = props;
+    console.log("hello")
+
+}
+
+const getApiData = () => {
+
+    const displayData = (data) => {
+        console.log(data);
+        data.map((item) => {
+            console.log(item);
+            return Task(item);
+        })
+    }
+
+    fetch(apiUrl).then((response) => response.json()).then(displayData);
+
+}
+
+getApiData();
