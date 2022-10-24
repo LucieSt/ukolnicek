caa214bd80c70a0b3211d0ac1ad8166228808da8
LucieSt <strakova.91@gmail.com>
2022-10-24 18:17:44 +0200

    finished with the task

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
-console.log('funguju!');
 
 const apiUrl = "https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks"
 
+const checkElm = document.getElementById("checkbox-undone");
+let checkCondition = false
+
 const Task = (props) => {
     const { id, name, due, done } = props;
-    console.log("hello")
 
-}
-
-const getApiData = () => {
-
-    const displayData = (data) => {
-        console.log(data);
-        data.map((item) => {
-            console.log(item);
-            return Task(item);
-        })
+    let doneSign = ""
+    if (done) {
+        doneSign = "✓";
     }
 
-    fetch(apiUrl).then((response) => response.json()).then(displayData);
+    return `
+    
+    <div class="task" id="${id}">
+        <div class="task__body">
+            <div class="task__name">${name}</div>
+            <div class="task__due">${due}</div>
+        </div>
+        <div class="task__done">${doneSign}</div>
+    </div>
+
+    `;
+}
+
+const getData = () => {
+
+    const renderTasks = (data) => {
+
+        const tasksElm = document.querySelector(".todo__tasks");
+        tasksElm.innerHTML = data.map((item) => {
+                if (checkCondition) {
+                    if (!item.done) {
+                        return Task(item);
+                    }
+                }
+                else {
+                    return Task(item);
+                }
+        }).join('');
+    }
+
+    fetch(apiUrl).then((response) => response.json()).then(renderTasks);
 
 }
 
-getApiData();
+getData()
+
+
+checkElm.addEventListener('change', function() {
+    if (this.checked) {
+      checkCondition = true
+      getData()
+    } else {
+      checkCondition = false
+      getData()
+    }
+  });
